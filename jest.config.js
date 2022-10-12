eb2e045d353dd47473940894ea09809f736325f5
Ken Fukuyama <kenfdev@gmail.com>
2022-10-12 11:31:26 +0900

    modify jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts-esm',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   testEnvironment: 'node',
-
-  // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
 };
