cbdab6645628b6dae1902144c8ce6e2d389f4d6f
Ken Fukuyama <kenfdev@gmail.com>
2022-10-14 10:28:16 +0900

    fix config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts-esm',
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
+  transformIgnorePatterns: ["/node_modules/(?!@planetscale)"],
   testEnvironment: 'node',
 };
